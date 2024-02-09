af5035694b908bad9378075615c8146623fc555e
Andreas Luemkemann <andreas@infraspread.net>
2024-02-09 18:17:36 +0100

    modified:   html/rustdeskdl_win.ps1

--- html/rustdeskdl_win.ps1
+++ html/rustdeskdl_win.ps1
@@ -29,6 +29,38 @@ image_quality = 'low'
 enable_file_transfer = 'Y'
 '@
 
+Function Check-RunAsAdministrator()
+{
+  #Get current user context
+  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  
+  #Check user is running the script is member of Administrator Group
+  if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
+  {
+       Write-host "Script is running with Administrator privileges!"
+  }
+  else
+    {
+       #Create a new Elevated process to Start PowerShell
+       $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+ 
+       # Specify the current script path and name as a parameter
+       $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+ 
+       #Set the Process to elevated
+       $ElevatedProcess.Verb = "runas"
+ 
+       #Start the new elevated process
+       [System.Diagnostics.Process]::Start($ElevatedProcess)
+ 
+       #Exit from the current, unelevated, process
+       Exit
+ 
+    }
+}
+ 
+
+
 write-host "Main Time: $(Get-Date)" -ForegroundColor Yellow
 
 function RustdeskWaitService {
@@ -193,6 +225,7 @@ function get-GithubRelease {
         #        irm -Uri $($_.URL) -OutFile .\$Destination\$($_.File)
     }
 }
-
+#Check Script is running with Elevated Privileges
+Check-RunAsAdministrator
 get-GithubRelease @DownloadControl -Destination $targetdir
 RustdeskMenu -RustdeskUpdateExe $RustdeskUpdateExe -InstallOrUpgrade -Configure
