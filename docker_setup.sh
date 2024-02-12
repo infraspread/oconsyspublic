7d6b35fa5aa7cf99317060b9c9dfa08948473022
Infraspread UG <138169104+andreasluemkemann@users.noreply.github.com>
2024-02-12 15:46:27 +0100

    Create docker_setup.sh

---
+++ docker_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# usage (as root): curl -fsSL https://github.com/infraspread/oconsyspublic/docker_setup.sh -o docker_setup.sh
+su -
+apt-get update
+apt-get install sudo curl -y
+cd ~
+curl -fsSL https://get.docker.com -o install-docker.sh
+sudo sh install-docker.sh
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.24.5/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
